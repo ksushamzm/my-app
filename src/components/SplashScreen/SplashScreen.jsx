156e10943d271439ac10e14e7958413ce8996ccd
ksushamzm <kalemudkina@edu.hse.ru>
2024-11-30 02:42:36 +0300

    мяу

--- src/components/SplashScreen/SplashScreen.jsx
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -55,5 +55,5 @@ const SplashScreen = () => {
 export default SplashScreen;
 
 //<div className="pattern">
-//<img src="./images/pattern.svg" className="patterrn" />
+//<img src="./images/pattern.svg" className="patten" />
 //</div>
